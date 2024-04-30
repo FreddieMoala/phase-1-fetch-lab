9a737c2a400b68baba93566bccc454ba09e8ed90
Siaosi Moala <loto_toa@hotmail.com>
2024-04-30 13:34:48 +1000

    Done

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(response => response.json())
   .then(json => renderBooks(json));
-  return fetch('https://anapioficeandfire.com/api/books');
-  
-  
-}
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -17,6 +14,10 @@ function renderBooks(books) {
   });
 }
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// console.log(fetchBooks());
